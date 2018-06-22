658728cf81aa81d6b554a8c439c9030a1dbf3b1e
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-06-22 14:08:59 -0700

    Added a short explenation of the temporary scenarioTest files purpose

--- seedSrc/scenarioTest.js
+++ seedSrc/scenarioTest.js
@@ -1,4 +1,16 @@
 
+/**
+ * Runs test scenarios to be called by the clientSrc/wallet.js files for testing the internal seedSrc code.
+ * 
+ * Unit testing is great, however these are to test scenarios.
+ * 
+ * Exports:
+ *      seedModuleTest() - Runs the Seed Module test scenario and prints to the console along the way
+ *      cryptographyTest() - Runs the cryptography test scenario and invokes the unit tests
+ *      vmModuleTest() - Runs a simple module creation and execution test [Deprecated in favour of seedModuleTest]
+ *      
+ */
+
 const cryptoHelperExporter = require("./cryptoHelper.js");
 const accountExporter = require("./account.js");
 const cryptographyHelper = cryptoHelperExporter.newCryptoHelper();
@@ -7,63 +19,9 @@ const moduleExporter = require("./module.js");
 const seedExporter = require("./seed.js");
 
 module.exports = {
-    cryptographyTest: function() {
-        console.log("Cryptography Test");
-        let newAccount = accountExporter.newAccount({ entropy : "1349082123412353tgfdvrewfdvdfr43f34390413290413", network : "00" });
-        if (newAccount.privateKey == null) {
-            console.log("NO PRIVATE KEY");
-        }
-        console.info(newAccount);
-        let signature = newAccount.sign("dataToSign");
-        console.log(signature);
-        accountExporter.runUnitTests();
-        cryptoHelperExporter.runUnitTests();
-        console.log("Crypto Test Complete");
-    },
-    vmModuleTest : function() {
-        console.log("Module Test::Create Game");
-        let vm = virtualMachineExporter.createVirtualMachine();
-        let game = moduleExporter.createModule({
-            module : "Game", 
-            data : { walls : [[1, 1, 1], [0, 0, 0], [0, 0, 0]] },
-            initialUserData : { x : 2, y : 1 }
-        });
-        
-        let moveLeft = function(container, changeContext) {
-            let gameData = container.getModuleData("Game");
-            let userData = container.getUserData("Game", container.sender);
-            let walls = gameData["walls"];
-            if (walls[userData["x"] - 1][userData["y"]] == 0) {
-                changeContext.subtract(1, { user : container.sender, key : "x" });
-            }
-            return changeContext;
-        }
-        
-        game.addFunction({
-            invoke : moveLeft, 
-            name : "moveLeft"
-        });
-        
-        console.log("Add Game to VM");
-        vm.addModule(game);
-        console.log("Add user to Game in VM");
-        vm.addUser({module : "Game"}, "ABC");
-        console.info("User Data In Module (Default)", vm.getModule({module : "Game"}).data["userData"]);
-        
-        console.log("Invoke \"MoveLeft\" function on user");
-        let changeContext = vm.simulate({module : "Game", function : "moveLeft", user : "ABC"});
-        console.info("Changes caused by \"MoveLeft\"", changeContext);
-        vm.applyChangeContext({module : "Game"}, changeContext);
-        console.info("User data after moving left the first time", vm.getModule({module : "Game"}));
-    
-        console.log("Invoke \"MoveLeft\" function on user");
-        changeContext = vm.simulate({module : "Game", function : "moveLeft", user : "ABC"});
-        console.info("Changes caused by \"MoveLeft\"", changeContext);
-        vm.applyChangeContext({module : "Game"}, changeContext);
-        console.info("User data after moving left the second time", vm.getModule({module : "Game"}));
-
-        console.log("VM Test Complere");
-    },
+    /**
+     * Runs the Seed Module test scenario, confirming the Seed cryptocurrency module behaves as expected
+     */
     seedModuleTest : function() {
         console.log("SeedModuleTest::Start")
 
@@ -118,5 +76,68 @@ module.exports = {
         console.log(vm.invoke({ module : "Seed", function : "burn", user : "DEF", args : {value : 25 } }));
 
         console.info(seedModule.data);
+    },
+    /**
+     * Runs the Cryptography and account test scenario
+     */
+    cryptographyTest: function() {
+        console.log("Cryptography Test");
+        let newAccount = accountExporter.newAccount({ entropy : "1349082123412353tgfdvrewfdvdfr43f34390413290413", network : "00" });
+        if (newAccount.privateKey == null) {
+            console.log("NO PRIVATE KEY");
+        }
+        console.info(newAccount);
+        let signature = newAccount.sign("dataToSign");
+        console.log(signature);
+        accountExporter.runUnitTests();
+        cryptoHelperExporter.runUnitTests();
+        console.log("Crypto Test Complete");
+    },
+    /**
+     * Runs a basic virtual machine test scenario
+     */
+    vmModuleTest : function() {
+        console.log("Module Test::Create Game");
+        let vm = virtualMachineExporter.createVirtualMachine();
+        let game = moduleExporter.createModule({
+            module : "Game", 
+            data : { walls : [[1, 1, 1], [0, 0, 0], [0, 0, 0]] },
+            initialUserData : { x : 2, y : 1 }
+        });
+        
+        let moveLeft = function(container, changeContext) {
+            let gameData = container.getModuleData("Game");
+            let userData = container.getUserData("Game", container.sender);
+            let walls = gameData["walls"];
+            if (walls[userData["x"] - 1][userData["y"]] == 0) {
+                changeContext.subtract(1, { user : container.sender, key : "x" });
+            }
+            return changeContext;
+        }
+        
+        game.addFunction({
+            invoke : moveLeft, 
+            name : "moveLeft"
+        });
+        
+        console.log("Add Game to VM");
+        vm.addModule(game);
+        console.log("Add user to Game in VM");
+        vm.addUser({module : "Game"}, "ABC");
+        console.info("User Data In Module (Default)", vm.getModule({module : "Game"}).data["userData"]);
+        
+        console.log("Invoke \"MoveLeft\" function on user");
+        let changeContext = vm.simulate({module : "Game", function : "moveLeft", user : "ABC"});
+        console.info("Changes caused by \"MoveLeft\"", changeContext);
+        vm.applyChangeContext({module : "Game"}, changeContext);
+        console.info("User data after moving left the first time", vm.getModule({module : "Game"}));
+    
+        console.log("Invoke \"MoveLeft\" function on user");
+        changeContext = vm.simulate({module : "Game", function : "moveLeft", user : "ABC"});
+        console.info("Changes caused by \"MoveLeft\"", changeContext);
+        vm.applyChangeContext({module : "Game"}, changeContext);
+        console.info("User data after moving left the second time", vm.getModule({module : "Game"}));
+
+        console.log("VM Test Complere");
     }
  }
