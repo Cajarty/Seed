56c215e64cba0cc227651520b6ceadcf680de8bd
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-07-30 16:55:22 -0700

    Commented messaging system

--- seedSrc/messaging.js
+++ seedSrc/messaging.js
@@ -7,7 +7,7 @@
  * Exported Functions:
  *      subscribeToFunctionCallback(moduleName, functionName, callback)
  *      subscribeToDataChange(moduleName, key, user?, callback)
- *      unsubscribe()
+ *      unsubscribe(moduleName, funcNameOrDataKey, receipt, optionalUser?)
  *      invoke(moduleName, functionName, transactionHash, changeSet)
  */
 
@@ -27,11 +27,14 @@
 
 module.exports = {
     /**
+     * Subscribes for callback to be invoked whenever the given module has the given function validated
+     * by the system.
      * 
+     * @param moduleName - The name of the module were subscribing to
+     * @param functionName - The name of the function were subscribing to
+     * @param callback - The callback function being invoked
      * 
-     * @param a - 
-     * 
-     * @return - 
+     * @return - The receipt regarding where the subscription was stored
      */
     subscribeToFunctionCallback : function(moduleName, functionName, callback) {
         let key = moduleName + functionName;
@@ -43,11 +46,18 @@ module.exports = {
         return "F" + receipt;
     },
     /**
+     * Subscribes for callback to be invoked whenever a given module or user data within the module
+     * has internal data changed by a function.
      * 
+     * When a user is passed in, the dataKey refers to a users data. If no user is passed in, it refers
+     * to the module itself.
      * 
-     * @param a - 
+     * @param moduleName - The name of the module were subscribing to
+     * @param dataKey - The variable name of the data we're subscribing to
+     * @param callback - The callback function being invoked
+     * @param user - (optional) The user which owns the data key
      * 
-     * @return - 
+     * @return - The receipt regarding where the subscription was stored
      */
     subscribeToDataChange : function(moduleName, dataKey, callback, user) {
         let key = moduleName + dataKey;
@@ -70,11 +80,13 @@ module.exports = {
         }
     },
     /**
+     * Unsubscribes callbacks from the messaging system to cleanup
      * 
+     * @param moduleName - The name of the module we were subscribed to
+     * @param funcNameOrDataKey - Either the function name or data key subscribed to
+     * @param receipt - The receipt returned during subscription
+     * @param optionalUser - (Optional) The user who owns the data key when subscribed
      * 
-     * @param a - 
-     * 
-     * @return - 
      */
     unsubscribe : function(moduleName, funcNameOrDataKey, receipt, optionalUser) {
         let firstChar = receipt[0];
@@ -92,11 +104,12 @@ module.exports = {
         }
     },
     /**
+     * Invokes callbacks for all subscribed callbacks whenever a function is invoked
      * 
-     * 
-     * @param a - 
-     * 
-     * @return - 
+     * @param moduleName - The name of the module that was invoked
+     * @param functionName - The name of the function that was invoked
+     * @param transactionHash - The transaction hash of the function that was just invoked
+     * @param changeSet - The changes altered to the ledger through the invoking
      */
     invoke : function(moduleName, functionName, transactionHash, changeSet) {
         // If we have a changeset and that changeset is an object
