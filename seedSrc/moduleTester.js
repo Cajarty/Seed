8ebaddb1a477da693d831ff370c7fa2f5fb45fff
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-08-10 13:12:56 -0700

    Commented the moduleTester.js code regarding unit testing and scenario testing

--- seedSrc/moduleTester.js
+++ seedSrc/moduleTester.js
@@ -1,3 +1,13 @@
+/*******************
+ * moduleTester.js *
+ *******************
+ * 
+ * Helper class for unit testing/scenario testing modules
+ * 
+ * Exported Functions:
+ *      beginTest(moduleName, startingUser, simulateDAG)
+ */
+
 const vmExporter = require("./virtualMachine/virtualMachine.js");
 const transactionExporter = require("./transaction.js");
 const entanglementExporter = require("./entanglement.js");
@@ -6,13 +16,34 @@ const accountExporter = require("./account.js");
 let vm = null;
 
 module.exports = {
+    /**
+     * Beings a test and creates a new ModuleTester class to return
+     * 
+     * If simulateDAG is true, relay transactions will be required to validate transactions. If its false, none will be required
+     * 
+     * @param moduleName - The name of the module to test on
+     * @param startingUser - The default user in the test
+     * @param simulateDAG - Whether to simulate DAG validation requirements or not
+     * 
+     * @return - The newly created ModuleTester for this test
+     */
     beginTest : function(moduleName, startingUser, simulateDAG) {
         vm = vmExporter.getVirtualMachine();
         return new ModuleTester(moduleName, startingUser, simulateDAG);
     } 
 }
 
+/**
+ * The wrapper class for testing modules
+ */
 class ModuleTester{
+    /**
+     * The constructor for ModuleTester which begins the test and assigns the default variables
+     * 
+     * @param moduleName - The name of the module to test on
+     * @param startingUser - The default user in the test
+     * @param simulateDAG - Whether to simulate DAG validation requirements or not
+     */
     constructor(moduleName, startingUser, simulateDAG) {
         console.info("Begin Test For Module " + moduleName);
         this.moduleName = moduleName;
@@ -25,11 +56,21 @@ class ModuleTester{
         this.simulateDAG = simulateDAG;
     }
 
+    /**
+     * Invoked when a test is passed, this increases updates the tallies for the passing tests
+     */
     passTest() {
         this.passCounter++;
         this.totalCounter++;
     }
 
+    /**
+     * Invoked when a test fails, this creates a fail message and adds it to a list of fail messages. These messages are all displayed once
+     * the unit tests terminate. Also increases teh totalCounter tests tally
+     * 
+     * @param {*} failMessage - The message to display upon failing
+     * @param {*} actualResult - The actual result for whatever test failed, rather than the expected one
+     */
     failTest(failMessage, actualResult) {
         let msg = "Failed Test# " + this.totalCounter + " \n-" + failMessage;
         if (actualResult != undefined) {
@@ -39,6 +80,11 @@ class ModuleTester{
         this.totalCounter++;
     }
 
+    /**
+     * Changes which user is sending the follow transactions
+     * 
+     * @param {*} newUser - A string representing the new user to send transactions ons behalf
+     */
     switchUser(newUser) {
         if (this.accounts[newUser] == undefined) {
             this.accounts[newUser] = accountExporter.newAccount( { entropy : newUser + "_123456789012345678901234567890", network : "00" });
@@ -46,6 +92,11 @@ class ModuleTester{
         this.currentUser = this.accounts[newUser];
     }
 
+    /**
+     * Returns the Account object related to a given user's string/name
+     * 
+     * @param {*} user - The name of the user to fetch
+     */
     getAccount(user) {
         if (this.accounts[user] == undefined) {
             this.accounts[user] = accountExporter.newAccount( { entropy : user + "_123456789012345678901234567890", network : "00" });
@@ -53,6 +104,12 @@ class ModuleTester{
         return this.accounts[user].publicKey;
     }
 
+    /**
+     * Invokes a module's function execution in the virutal machine
+     * 
+     * @param {*} functionName - The name of the function to invoke
+     * @param {*} args - The arguments passed in as parameters
+     */
     invoke(functionName, args) {
         return vm.invoke({ 
             module : this.moduleName, 
@@ -63,21 +120,39 @@ class ModuleTester{
         });
     }
 
+    /**
+     * Creates a relay transaction to push validation forward in the simulation. Creates a random user to do the transaction
+     * 
+     * @return - The transaction created
+     */
     relay() {
         let oldAccount = this.currentUser;
-        let name = "Z" + Math.floor((Math.random() * 10));
+        let name = "Z" + (Math.random() * 100) % 100;
         this.switchUser(name);
         let transaction = vm.createTransaction(this.currentUser, "Relay", "relay", {}, this.simulateDAG ? 4 : 0);
         this.currentUser = oldAccount;
         return transaction;
     }
 
+    /**
+     * Creates a transaction and runs it in the virtual machine
+     * 
+     * @param {*} functionName - The name of the function to execute
+     * @param {*} args - The arguments to be passed into the function as parameters
+     * 
+     * @return - The transaction created
+     */
     createTransaction(functionName, args) {
         return vm.createTransaction(this.currentUser, this.moduleName, functionName, args, this.simulateDAG ? 2 : 0);
     }
 
+    /**
+     * Tries to create a transaction, and if it can't due to congestion issues, it creates a relay if needed before trying again
+     * 
+     * @param {*} functionName - The name of the function to execute
+     * @param {*} args - The arguments to be passed into the function as parameters
+     */
     createTransactionWithRelay(functionName, args) {
-        //console.info("CreatingTransaction", functionName, args);
         let transaction = this.createTransaction(functionName, args);
         if (transaction == null) {
             this.relay();
@@ -86,6 +161,14 @@ class ModuleTester{
         return transaction;
     }
 
+    /**
+     * Tries to create a transaction and invoke it in the virtual machine
+     * 
+     * @param {*} functionName - The name of the function to execute
+     * @param {*} args - The arguments to be passed into the function as parameters
+     * 
+     * @return - The changeset resulted from the invoked transaction
+     */
     createAndInvokeTransaction(functionName, args) {
         let transaction = this.createTransaction(functionName, args);
         let txHashes = [];
@@ -101,6 +184,15 @@ class ModuleTester{
         }, transaction.execution.changeSet);
     }
 
+    /**
+     * Asserts that, if we invoked the given function with its given arguments, what it returns will be equal to the valueToCheck
+     * If not, it will fail the test and log the failMessage
+     * 
+     * @param {*} functionName - The name of the getter to check
+     * @param {*} args - The arguments passed into the getter
+     * @param {*} valueToCheck - The expected value to get
+     * @param {*} failMessage - The fail message to log if the values do not match
+     */
     assertEqual(functionName, args, valueToCheck, failMessage) {
         let result = this.invoke(functionName, args);
         if (result == valueToCheck) {
@@ -112,6 +204,12 @@ class ModuleTester{
         }
     }
 
+    /**
+     * Asserts that, if we attepted to execute a transaction, it would fail
+     * 
+     * @param {*} functionName - The function that we want to test
+     * @param {*} args - The arguments passed into the function to test
+     */
     assertInvokeFailToChangeState(functionName, args) {
         let result = this.invoke(functionName, args);
         if (result == vm.ERROR.FailedToChangeState) {
@@ -123,6 +221,14 @@ class ModuleTester{
         }
     }
 
+    /**
+     * Asserts that, if we invoked the given function with its given arguments, what it returns will true
+     * If not, it will fail the test and log the failMessage
+     * 
+     * @param {*} functionName - The name of the getter to check
+     * @param {*} args - The arguments passed into the getter
+     * @param {*} failMessage - The fail message to log if the values do not match
+     */
     assertTrue(functionName, args, failMessage) {
         let result = this.invoke(functionName, args);
         if (!result) {
@@ -134,9 +240,12 @@ class ModuleTester{
         }
     }
 
+    /**
+     * Ends the test and logs the results to the console
+     */
     endTest() {
-        console.log("Test Complete For Module " + this.moduleName);
-        console.log("Result: " + this.passCounter + "/" + this.totalCounter);
+        console.log("### Test Complete For Module " + this.moduleName + " ###");
+        console.log("### Result: " + this.passCounter + "/" + this.totalCounter+ " ###") ;
         if (this.fails.length > 0) {
             console.log(this.fails);
         }
