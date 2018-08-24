d4b88e9ad56476c77ceff3a539e9cf5b0ec69102
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-08-24 12:07:48 -0700

    Commented the Relay DApp's index.js file

--- clientSrc/modules/relay/index.js
+++ clientSrc/modules/relay/index.js
@@ -1,17 +1,24 @@
-const seed = require("../../../seedSrc/index.js");
-const ipc = require('electron').ipcRenderer;
+/******************
+ * relay/index.js *
+ ******************
+ * 
+ * The JavaScript loaded into the Relay DApp.
+ * 
+ * Communicates with the SeedHLAPI to use the "Relay" module and relay an empty transaction. 
+ * These transactions are used to push the validation forward.
+ */
+
 const { PromiseIpc } = require('electron-promise-ipc');
 const promiseIpc = new PromiseIpc({ maxTimeoutMs: 2000 });
 const seedHLAPI = require("../../seedHLAPI.js").getSeedHLAPI(promiseIpc);
 
-let accounts = {};
-let currentUser = undefined;
-
+/**
+ * Simulates one of 100 random accounts sending a relay transaction through the Relay module
+ */
 function relay() {
     let entropy = ("Z" + (Math.random() * 100) % 100) + "_123456789012345678901234567890";
     seedHLAPI.switchAccount(entropy)
         .then(() => {
             seedHLAPI.createTransaction("Relay", "relay", {}, 4);
         });
-    //return transaction;
 }
