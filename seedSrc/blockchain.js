81d8bfcc2b8c8b901211055aebf8a89fca14cd43
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-08-28 22:35:48 -0700

    Commented the blockchain file

--- seedSrc/blockchain.js
+++ seedSrc/blockchain.js
@@ -1,11 +1,26 @@
-/*##############################################################
-Blockchain:
-##############################################################*/
+/*****************
+ * blockchain.js *
+ *****************
+ * 
+ * Exports access to the blockchain.
+ * 
+ * Adding a block adds it to the blockchain as a first generation block. If this block meets the requirements, it may trigger squashing,
+ * squashing first generation blocks into subsequent generations
+ * 
+ * Exported Functions:
+ *      addTestamentBlock(block)
+ *          - Adds a block to the blockchain, potentially triggering the squashing of blocks as well
+ */
 
 const squasherExporter = require("./squasher.js");
 
 
 module.exports = {
+    /**
+     * Adds a block to the blockchain, potentially triggering the squashing of blocks as well
+     * 
+     * @param {*} block - The block to add to the blockchains
+     */
     addTestamentBlock: function(block) {
         ensureCreated(block.generation);
         blockchain[block.generation].push(block);
@@ -15,25 +30,40 @@ module.exports = {
 
             deleteGenerationOfBlocks(block.generation);
 
-            ensureCreated(nextGenerationBlock.generation);
-            blockchain[nextGenerationBlock.generation].push(nextGenerationBlock);
+            this.addTestamentBlock(nextGenerationBlock);
         }
-        
+
         debugBlockchain();
     }
  }
 
+ // The mapping of blocks in the blockchain 
+ let blockchain = {}
+
+ /**
+  * Helper function for deleting a certain generation of blocks from the blockchain*
+  * 
+  * @param {*} generation - The generation to delete
+  */
  let deleteGenerationOfBlocks = function(generation) {
     blockchain[generation] = [];
     delete blockchain[generation];
  }
 
+ /**
+  * Helper function to ensure a certain generation's chain exists in the "blockchain" mapping 
+  *
+  * @param {*} generation - The generation to ensure exists
+  */
  let ensureCreated = function(generation) {
     if (!blockchain[generation]) {
         blockchain[generation] = [];
     }
  }
 
+ /**
+  * Helper function for printing to terminal the current state of the blockchain
+  */
  let debugBlockchain = function() {
      console.info("### Debug Blockchain ###")
      let keys = Object.keys(blockchain);
@@ -52,5 +82,3 @@ module.exports = {
          }
      }
  }
-
-let blockchain = {}
