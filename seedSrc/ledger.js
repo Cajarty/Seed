2253f93e04761392144b66d90320182684979c9f
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-08-29 19:48:24 -0700

    Commented logic regarding ledger using squasher

--- seedSrc/ledger.js
+++ seedSrc/ledger.js
@@ -60,57 +60,16 @@ module.exports = {
             }
         }
 
+        // Old module data we are overwriting
         let oldData = Object.assign(this.moduleData);
 
+        // Store the incoming changeContext into an object which follows the same schema
         let newData = {}
         newData[moduleName] = changeContext.moduleData;
         newData[moduleName]["userData"] = changeContext.userData;
 
         //Squash and save
         this.moduleData = squasherExporter.squash(oldData, newData);
-
-        /*let moduleDataToUpdate = this.getModuleData(moduleName);
-        for(let i = 0; i < moduleDataKeys.length; i++) {
-            let key = moduleDataKeys[i];
-            moduleDataToUpdate[key] += changeContext.moduleData[key]; //Only works cause number. Should check if number
-        }
-
-        for(let i = 0; i < users.length; i++) {
-            let user = users[i];
-            let userDataKeys = Object.keys(changeContext.userData[user]);
-
-            if (moduleDataToUpdate.userData[user] == undefined) {
-                this.addUserData(moduleName, user);
-            }
-
-            for(let j = 0; j < userDataKeys.length; j++) {
-                let key = userDataKeys[j];
-                let value = changeContext.userData[user][key];
-                switch(typeof value) {
-                    case "number":
-                        // Number changes are relative
-                        moduleDataToUpdate["userData"][user][key] += changeContext.userData[user][key];
-                        break;
-                    case "string":
-                        // Strings are absolute
-                        moduleDataToUpdate["userData"][user][key] = changeContext.userData[user][key]; 
-                        break;
-                    case "object":
-                        // Objets are absolute and Object.assigned over
-                        let innerKeys = Object.keys(changeContext.userData[user][key]);
-                        for(let k = 0; k < innerKeys.length; k++) {
-                            let userDataAtKey = moduleDataToUpdate["userData"][user][key];
-                            if (userDataAtKey[innerKeys[k]] == undefined || typeof userDataAtKey[innerKeys[k]] != "number") {
-                                userDataAtKey[innerKeys[k]] = changeContext.userData[user][key][innerKeys[k]];
-                            } else {
-                                userDataAtKey[innerKeys[k]] += changeContext.userData[user][key][innerKeys[k]];
-                            }
-                        }
-                        //moduleToUpdate.data["userData"][user][key] = Object.assign({}, changeContext.userData[user][key] );
-                        break;
-                }
-            }
-        }*/
     }
 
     revertChanges(changeSet) {
