5e348bbb522d905856103e82df2faed3322f70c2
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-09-02 11:44:43 -0700

    Created & commented skeleton for the LocalStorage implementation of iDatabaseInjector

--- seedSrc/storage/LocalStorageInjector.js
+++ seedSrc/storage/LocalStorageInjector.js
@@ -1,37 +1,201 @@
-
+/***************************
+ * localStorageInjector.js *
+ ***************************
+ * 
+ * Exports an implementation of the IDatabaseInjector pattern which implements saving/loading
+ * to a browsers localstorage
+ * 
+ */
 
 module.exports = {
+    /**
+     * Creates a new LocalStorageInjector implementation
+     * 
+     * @return - A new LocalStorageInjector object
+     */
     newLocalStorageInjector : function() {
         return new LocalStorageInjector();
     }
 }
 
 class LocalStorageInjector /* implements IDatabaseInjector.interface */ {
-    writeBlock(storageName, storageObject, generation) {
-        // returns bool
-    }
-
-    writeTransaction(storageName, storageObject) {
-        // returns bool
-    }
-
-    readBlock(storageName) {
+    /**
+     * Constructor for the local storage implementation
+     */
+    constructor() {
 
     }
 
-    readBlockchain(generation) {
-
+    /**
+     * Removes a transaction from storage asyncrhonously, invoking an optional
+     * callback upon completion
+     * 
+     * @param {*} transactionName - The name/hash of a transaction in storage
+     * @param {*} callback - (optional) A function(error, data) to invoke upon completetion
+     */
+    removeTransactionAsync(transactionName, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Remove transaction from local storage
     }
 
-    readBlockchains() {
-
+    /**
+     * Removes a block from storage asynchronously, invoking an optional
+     * callback upon completion
+     * 
+     * @param {*} generation - The generation of a block in storage
+     * @param {*} blockName - The name/hash of a block in storage
+     * @param {*} callback - (optional) A function(error, data) to invoke upon completetion
+     */
+    removeBlockAsync(generation, blockName, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Removes block from local storage
     }
 
-    readTransaction(storageName) {
-
+    /**
+     * Writes a block to storage asynchronously, invoking an optional
+     * callback upon completion
+     * 
+     * @param {*} storageName - The name to use in storage (e.g. block hash)
+     * @param {*} storageObject - The block to store in storage
+     * @param {*} generation - The generation of block it is
+     * @param {*} callback - (optional) A function(error, data) to invoke upon completetion
+     */
+    writeBlockAsync(storageName, storageObject, generation, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Write block to local storage
     }
 
-    readEntanglement() {
+    /**
+     * Writes a transaction to storage asynchronously, invoking an optional
+     * callback upon completion
+     * 
+     * @param {*} storageName - The name of the transaction to store
+     * @param {*} storageObject - The transaction to store
+     * @param {*} callback - (optional) A function(error, data) to invoke upon completetion
+     */
+    writeTransactionAsync(storageName, storageObject, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Write transaction to local storage
+    }
 
+    /**
+     * Reads a block from storage asynchronously, with a callback to invoke
+     * on once reading has finished
+     * 
+     * @param {*} generation - The generation of block it is
+     * @param {*} storageName - The name of the block in storage
+     * @param {*} callback - The callback to invoke upon finishing reading
+     */
+    readBlockAsync(generation, storageName, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Read block from local storage
+    }
+
+    /**
+     * Reads a block from storage synchronously, returning the block upon completion.
+     * 
+     * @param {*} generation - The generation of block it is
+     * @param {*} storageName - The name of the block in storage
+     */
+    readBlockSync(generation, storageName) {
+        // Read block from local storage and return it
+    }
+
+    /**
+     * Reads all blocks of a certain generation synchronously, returning as an array
+     * 
+     * @param {*} generation - The generation of blocks to be read
+     */
+    readBlockchainSync(generation) {
+        let blocks = [];
+        let files = getFiles(this.blockPath(generation));
+        for(let i = 0; i < files.length; i++) {
+            let blockHash = files[i].split(".")[0];
+            let result = this.readBlockSync(generation, blockHash);
+            blocks.push(result);
+        }
+        return blocks;
+    }
+
+    /**
+     * Reads all blockchains of all generations and returns them as a long array of blocks
+     */
+    readBlockchainsSync() {
+        let blocks = [];
+        let generations = getDirectories(this.blockPath());
+        for(let i = 0; i < generations.length; i++) {
+            let generation = parseInt(generations[i]);
+            blocks = blocks.concat(this.readBlockchainSync(generation));
+        }
+        return blocks;
+    }
+
+    /**
+     * Reads a transaction from storage asynchronously, invoking a callback upon completion.
+     * 
+     * @param {*} storageName - The name of the transaction in storage
+     * @param {*} callback - The callback to invoke upon finishing reading
+     */
+    readTransactionAsync(storageName, callback) {
+        if (!callback) {
+            callback = (err, data) => {
+                if (err) {
+                    throw err;
+                }
+            }
+        }
+        // Read transaction from local storage
+    }
+
+    /**
+     * Reads a transaction synchronously from storage and returns it
+     * 
+     * @param {*} storageName - The name of the transaction in storage
+     */
+    readTransactionSync(storageName) {
+        // Read transaction from local storage and return it
+    }
+
+    /**
+     * Reads the entanglement from storage synchronously, returning all transactions read
+     */
+    readEntanglementSync() {
+        let transactions = [];
+        let files = getFiles(this.transactionPath());
+        for(let i = 0; i < files.length; i++) {
+            let transactionHash = files[i].split(".")[0];
+            transactions.push(this.readTransactionSync(transactionHash));
+        }
+        return transactions;
     }
 }
